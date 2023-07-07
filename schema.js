8e6648d8567915ce5824616f4c4fd634293fcf09
andreenakashima <andreenakashima@gmail.com>
2023-07-07 15:42:15 -0300

    Related Data

--- schema.js
+++ schema.js
@@ -3,17 +3,24 @@ export const typeDefs = `#graphql
     id: ID!
     title: String!
     platform: [String!]!
+    reviews: [Review!]
   }
+
   type Review {
     id: ID!
     rating: Int!
     content: String!
+    game: Game!
+    author: Author!
   }
+
   type Author {
     id: ID!
     name: String!
     verified: Boolean!
+    reviews: [Review!]
   }
+  
   type Query {
     reviews: [Review]
     review(id: ID!): Review
